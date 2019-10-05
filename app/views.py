227b83ea419b58a9c80f119660db954121a600a2
bscheibel <beate.scheibel@gmail.com>
2019-10-05 09:12:14 +0200

    pdf viewing does not work so far

--- app/views.py
+++ app/views.py
@@ -13,7 +13,7 @@ import base64
 
 
 #UPLOAD_FOLDER = '/Users/beatescheibel/Desktop/flask/uploads'
-UPLOAD_FOLDER = '/home/bscheibel/uploads_app'
+UPLOAD_FOLDER = "./static/"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'PDF'])
 
@@ -25,7 +25,7 @@ def allowed_file(filename):
 def convert_pdf_img(filename):
     PDFFILE = UPLOAD_FOLDER +"/" + filename
     subprocess.call(['pdftoppm', '-jpeg', '-singlefile',
-                     PDFFILE, '/home/bscheibel/uploads_app/out'])
+                     PDFFILE, '/home/bscheibel/PycharmProjects/app/app/temporary/out'])
 
 def extract_all(uuid, filename, db):
     #order_bounding_boxes_in_each_block.main(uuid, UPLOAD_FOLDER + "/" + filename)
@@ -53,7 +53,7 @@ def upload_file():
 
 @app.route('/show/<filename>&<uuid>')
 def uploaded_file(filename, uuid):
-    file_out = "out.jpg"
+    file_out = "temporary/out.jpg"
     #file_out = filename
     if request.method == 'POST':
         uuid = 433
@@ -70,28 +70,29 @@ def uploaded_file(filename, uuid):
         for dim in dims:
             html_code += '''<td><h4>''' + dim + '''</h4></td>'''
             for d in dims[dim]:
-                number = d
-
-                """ number = re.search(reg, d)
-                number = number.group(1)
                 try:
-                    floats = len(number.split(".")[1])
-                    if floats <= 1:
+                    number = re.search(reg, d)
+                    number = number.group(1)
+                    try:
+                        floats = len(number.split(".")[1])
+                        if floats <= 1:
+                            steps = 0.1
+                        elif floats == 2:
+                            steps = 0.01
+                        elif floats == 3:
+                            steps = 0.001
+                        else:
+                            steps = 0.001
+                    except:
                         steps = 0.1
-                    elif floats == 2:
-                        steps = 0.01
-                    elif floats == 3:
-                        steps = 0.001
-                    else:
-                        steps = 0.001
-                except:          """
-                steps = 0.1
+                except:
+                    number = d
                 coords = ",".join(str(e) for e in dims[dim][d])
                 html_code += "<tr><td style='text-align:center'> <input type='checkbox' name='relevant." + d + "' value='checked'> </td>" + \
                              "<td style='text-align:center'>" + d + "</td>" + \
                              "<td style='text-align:center'> <input type='number' step='" + str(steps) + "' data-coords='" + coords + "' name='" + d + "' value='" + number + "'  size='10'> </td></tr>"
                 #print(html_code)
-        return render_template('show_image.html', filename=file_out, isos=isos, dims=dims, text=html_code, number=number_blocks)
+        return render_template('show_image.html', filename=file_out, isos=isos, dims=dims, text=html_code, number=number_blocks, og_filename=filename)
 
     else:
         filename = filename
@@ -130,16 +131,23 @@ def redis_get(key):
     result = json.loads(db.get(key))
     return result
 
-@app.route('/redis/set/<key>/<value>',methods=['POST'])
-def redis_set(key, value):
+@app.route('/redis/set/<key>',methods=['POST'])
+def redis_set(key):
+    value = request.get_json(force=True)
+    value = value["value"]
     db = redis.Redis("localhost")
+
+    value_name = value[0]
+    value_v = value[1]
     try:
         result = json.loads(db.get(key))
-
-
-        key = key.encode("utf-8")
-        result = db.set(key, str(value))
-    else:
-        db.set( )
+        result[value_name] = value_v
+        json_res = json.dumps(result)
+        db.set(key,json_res)
+    except:
+        dict_res = {}
+        dict_res[value_name] = value_v
+        json_dict = json.dumps(dict_res)
+        db.set(key, json_dict)
     return "OK"
 
