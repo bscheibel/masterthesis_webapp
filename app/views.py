25d721da5268f4564dcce632af7a41226f642fab
bscheibel <beate.scheibel@gmail.com>
2020-01-13 11:19:19 +0100

    cleaned up

--- app/views.py
+++ app/views.py
@@ -5,8 +5,6 @@ import subprocess
 import redis
 import random
 import PyPDF2
-import json
-import base64
 import os
 import json
 import re
@@ -50,19 +48,6 @@ def get_file_size(file):
     print(w,h,OrientationDegrees)
     return w,h, orientation
 
-
-
-
-def check_config_file(d):
-    reg_search = d
-    #print(reg_search)
-
-    for conf in d:
-        print(conf)
-
-    return "blub"
-
-
 def check_links(isos):
     link_names = {}
     isos_names = []
@@ -73,7 +58,6 @@ def check_links(isos):
     for name in isos:
         if re.search(reg_isos, name):
             n = 1
-            #print(name)
             new_isos = re.search(reg_isos,name).group(1)
             number = re.search(reg_isos,name).group(2)
             while n <= int(number):
@@ -84,16 +68,11 @@ def check_links(isos):
     for name in isos_new:
         try:
             name = name.replace(" ", "")
-            #name = name.replace("-"," ")
             url1 = name + ".PDF"
-            #print(url)
-            file = send_from_directory("static/isos",url1)
+            #file = send_from_directory("static/isos",url1)
             url = "isos/" + url1
-            #link_names.append(url)
             link_names[name] = url
-            #print(link_names)
         except:
-            #print(name)
             isos_names.append(name)
     return link_names, isos_names
 
@@ -122,16 +101,10 @@ def upload_file():
 @app.route('/show/<filename>&<uuid>')
 def uploaded_file(filename, uuid):
     file_out = "out.jpg"
-    #file_out = filename
-    #if request.method == 'POST':
-    #    uuid = 433
     if filename.endswith(".pdf") or filename.endswith(".PDF"):
         w,h, orientation = get_file_size(UPLOAD_FOLDER +"/" + filename)
         convert_pdf_img(filename)
         db = redis.Redis(unix_socket_path='/tmp/redis.sock',db=7)
-        #db = redis.Redis("unix_socket_path='127.0.0.1',6379,db=7")
-        #isos = db.get(uuid+"dims")
-        #print(iso)
         gen_tol = db.get(str(uuid)+"tol")
         print(gen_tol)
         isos = json.loads(db.get(str(uuid)+"isos"))
@@ -142,17 +115,13 @@ def uploaded_file(filename, uuid):
         html_code = "General tolerances according to: " + str(gen_tol) + "<br>"
         html_general = ""
         reg = r"(-?\d{1,}\.?\d*)"
-        #re_gewinde = r"M\d{1,2}"
-        #re_passungen = r"h\d{1,2}|H\d{1,2}"
         det_coords= "0,0,0,0"
         with open(path +'/static/config.json') as f:
             config_file = json.load(f)
             print(config_file)
 
         for dim in sorted(dims):
-            #print(dim)
             for det in details:
-                #print(det)
                 try:
                     if dim == det:
                         det_coords = details[det]
@@ -169,16 +138,6 @@ def uploaded_file(filename, uuid):
                 relevant_isos = []
                 search_terms = {}
                 terms = ''
-                #if "Ra" in d or "Rz" in d or "Rpk" in d:
-                #    relevant_isos.append("ISO4287.PDF")
-                #if u"\u27C2" in d or u"\u25CE" in d or u"\u232D" in d or u"\u2225" in d or u"\u232F" in d or u"\u2316" in d or u"\u2313" in d or u"\u23E5" in d:
-                #    relevant_isos.append("ISO1101.PDF")
-                #if re.search(re_gewinde,d):
-                #    relevant_isos.append("ISO6410.PDF")
-                #if "GG" in d or "CT" in d or "GX" in d:
-                #    relevant_isos.append("ISO14405-1.PDF")
-                #if re.search(re_passungen,d):
-                #    relevant_isos.append("ISO286-1.PDF")
                 for conf in config_file:
                     if re.search(conf,d):
                         iso = config_file[conf]
@@ -187,13 +146,10 @@ def uploaded_file(filename, uuid):
                             for blub in iso[key]:
                                 search_terms[blub] = iso[key][blub]
 
-                        #terms = '{"Symbole":1,"Tabelle":2,"Definition":3}'
                         if len(search_terms) < 1:
                             search_terms["Beginn"] = 1
                         terms = json.dumps(search_terms)
-                        #print(terms)
                         terms = base64.b64encode(terms.encode()).decode('utf-8')
-                        #terms = "blub"
 
 
 
@@ -222,31 +178,21 @@ def uploaded_file(filename, uuid):
 
                 relevant_isos = list(set(relevant_isos))
                 for x in relevant_isos:
-                    #html_code += "<td style='text-align:left'> <a href=" + url_for('static', filename="isos/"+x) + " >"+ x.partition(".")[0]  +"</a>  </td>"
                     html_code += "<td style='text-align:left' data-terms='" + terms + "'> <a onclick=ui_add_tab_active('#main','" + x.partition(".")[0] + "','" + x.partition(".")[0] +"',true,'isotab','"+terms+"')>" + x.partition(".")[0] + "</a>  </td>"
-                #print(html_code)
                 html_code += "</tr>"
                 html_links = ""
                 for link in links:
                     html_links += "<a onclick =ui_add_tab_active('#main','" + link + "','" + link +"',true,'isotab','empty')> Open " + link + "</a> <br>"
-                    #html_links += "<tr> <td> <a onclick =ui_add_tab_active('#main','iso1','iso1',true,'isotab')> Open " + link + " in Tab </a> </td> </tr>"""
-        #print("teeest")
         return render_template('index.html', filename=file_out, isos=isos, dims=dims, text=html_code,html_general=html_general, number=number_blocks, og_filename=filename, w=w, h=h, html_links=html_links, isos_names=isos_names, orientation=orientation)
-    #return render_template('test_pdfjs_textlayer.html', og_filename=filename)
-
-    #else:
-    #    filename = filename
-    #    return render_template('show_image.html', filename=filename)
 
 
 @app.route('/uploads/<filename>')
 def send_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
 
-# No caching at all for API endpoints.
+
 @app.after_request
 def add_header(response):
-    # response.cache_control.no_store = True
     response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
     response.headers['Pragma'] = 'no-cache'
     response.headers['Expires'] = '-1'
@@ -282,10 +228,3 @@ def redis_set(key):
         json_dict = json.dumps(dict_res)
         db.set(key, json_dict)
     return "OK"
-
-
-
-@app.route('/index')
-def test():
-    return render_template('index.html')
-
